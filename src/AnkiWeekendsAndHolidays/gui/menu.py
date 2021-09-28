2d7b20cdab96b2842d35b8883879813d73530a2f
RisingOrange <jakub.fidler@protonmail.com>
2021-09-28 20:07:17 +0200

    Dont add menu if it already exists

--- src/AnkiWeekendsAndHolidays/gui/menu.py
+++ src/AnkiWeekendsAndHolidays/gui/menu.py
@@ -6,6 +6,8 @@ from .anking_menu import get_anking_menu
 
 def setup_anking_menu() -> None:
     menu = get_anking_menu()
-    a = QAction("Weekends and Holidays", menu)
-    menu.addAction(a)
-    a.triggered.connect(conf.open_config)
+    action_text = "Weekends and Holidays"
+    if not any([child.text() == action_text for child in menu.findChildren(QAction)]):
+        a = QAction(action_text, menu)
+        menu.addAction(a)
+        a.triggered.connect(conf.open_config)
